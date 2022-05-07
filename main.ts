0a3f743164cb36e6e49aada9b50943dbf774829d
toyomasa1031 <76829611+toyomasa1031@users.noreply.github.com>
2022-05-07 17:12:01 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,20 @@
-let 角度 = 90
+let 角度 = 70
 pins.servoWritePin(AnalogPin.P1, 角度)
 basic.forever(function () {
-    basic.pause(3000)
-    for (let index = 0; index < 90; index++) {
-        角度 += -1
-        pins.servoWritePin(AnalogPin.P1, 角度)
-        basic.pause(10)
-    }
-    basic.pause(3000)
-    for (let index = 0; index < 90; index++) {
-        角度 += 1
-        pins.servoWritePin(AnalogPin.P1, 角度)
-        basic.pause(10)
+    if (input.lightLevel() <= 10) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+        for (let index = 0; index < 80; index++) {
+            角度 += -1
+            pins.servoWritePin(AnalogPin.P1, 角度)
+            basic.pause(10)
+        }
+        basic.pause(500)
+        for (let index = 0; index < 20; index++) {
+            角度 += 4
+            pins.servoWritePin(AnalogPin.P1, 角度)
+            basic.pause(10)
+        }
+        basic.clearScreen()
     }
 })
